81a498cc5a589e8078b3c2d32044c6e81049201f
kidistbezabih <kidistbezabh@gmail.com>
2022-07-07 22:41:29 +0300

    signup autentication with out checking validation

--- src/signupauth.php
+++ src/signupauth.php
@@ -4,10 +4,9 @@ if(isset($_POST['submit'])){
     $email_phone = $_POST['email/phone'];
     $name = $_POST['name'];
     $password = $_POST['password'];
-    $query = "INSERT INTO Users (
-        id, 
-        $name, 
-        $email_phone, 
-        $password
-        ); 
-}
+    $query = "INSERT INTO Users ( 
+        name, email/phone, password
+        ) VALUES ($name, $email_phone, $password);";
+        mysqli_query($con, $query);
+        //then redirect to some page
+};
