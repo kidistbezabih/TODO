6db6d401d4e97b0945092ca64f59d5d535221f42
Mikias Yonas <mikiasyonas512@gmail.com>
2022-07-23 23:52:21 +0300

    added name value for the frontend forms

--- public/pages/auth/signin.php
+++ public/pages/auth/signin.php
@@ -31,6 +31,7 @@ include "../../../src/utils/errorMessages.php";
                 </span>
                   <input
                     type="text"
+                    name="email"
                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Email address"
                   />
@@ -43,6 +44,7 @@ include "../../../src/utils/errorMessages.php";
                 </span>
                   <input
                     type="password"
+                    name="password"
                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Password"
                   />
