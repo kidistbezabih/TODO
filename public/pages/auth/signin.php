56917759762a25062c57d248850b752d7e99c5c5
Mikias Yonas <mikiasyonas512@gmail.com>
2022-07-27 11:53:09 +0300

    gave name for the forms

--- public/pages/auth/signin.php
+++ public/pages/auth/signin.php
@@ -61,6 +61,7 @@ include "../../../src/utils/errorMessages.php";
                 <!-- Submit button -->
                 <button
                   type="submit"
+                  name="login"
                   class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
