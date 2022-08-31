8812d5768e315838ab05bed37c26bdf46cddd84d
kidistbezabih <kidistbezabh@gmail.com>
2022-08-31 17:45:37 +0300

    function of todo

---
+++ src/controllers/todo.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../database/db.php';
+include_once '../utils/validators.php';
+include_once '../utils/session.php';
+include_once '../services/user.php';
+include_once '../services/todo.php';
+
+$status = $_POST['status'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+$planned_start_date = $_POST['planned_start_date'];
+$created_at = $_POST['created_at'];
+$planned_end_date = $_POST['planned_end_date'];
+$actual_start_date = $_POST['actual_start_date'];
+$actual_end_date = $_POST['actual_end_date'];
+
+if (isset($_POST['create_task'])) {
+  
+}
---
+++ src/services/todo.php
@@ -0,0 +1,82 @@
+<?php
+include_once "../database/db.php";
+include_once '../utils/session.php';
+
+function createTask($user_id, $status, $title, $description, 
+                    $created_at, $planned_start_date, 
+                    $planned_end_date, $actual_start_date, 
+                    $actual_end_date) 
+{
+    global $con;
+    $query = "INSERT INTO Tasks
+        (user_id, status, title, description,
+        created_at, planned_start_date, 
+        planned_end_date, actual_start_date, actual_end_date)
+        VALUES ($user_id, $status, $title, $description, 
+        $created_at, $planned_start_date, 
+        $planned_end_date, $actual_start_date, 
+        $actual_end_date);";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function edit($user_id, $title, $description, 
+            $planned_start_date, $planned_end_date) {
+    global $con;
+    $query = "UPDATE Tasks set title = $title, description = $description, 
+        planned_start_date = $planned_start_date, 
+        planned_end_date = $planned_end_date WHERE id = $user_id;";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user; 
+}
+
+function deleteTask($task_id) {
+    global $con;
+    $query = "DELETE FROM Tasks WHERE id = $task_id;";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function updatStatus($user_id,$status) {
+    global $con;
+    $query = "UPDATE Tasks SET status=$status WHERE id=$user_id;";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function getTask($user_id) {
+    global $con;
+    $query = "SELECT * FROM WHERE id= $user_id";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function getNotCompletedTasks($user_id) {
+    global $con;
+    $query = "SELECT * FROM WHERE id= $user_id && status='not completed'";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function getCompletedTasks($user_id) {
+    global $con;
+    $query = "SELECT * FROM Tasks WHERE id= $user_id && status='completed'";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function addFeedback($comment, $task_id) {
+    global $con;
+    $query = "UPDATE Tasks SET comment=$comment 
+    WHERE id=$task_id;";
+    $result = mysqli_query($con, $query);
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
