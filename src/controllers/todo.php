aff3e43b19665881d8c839548afd833d82440aec
kidistbezabih <kidistbezabh@gmail.com>
2022-09-01 12:10:58 +0300

    todo controllers

--- src/controllers/todo.php
+++ src/controllers/todo.php
@@ -13,8 +13,46 @@ $created_at = $_POST['created_at'];
 $planned_end_date = $_POST['planned_end_date'];
 $actual_start_date = $_POST['actual_start_date'];
 $actual_end_date = $_POST['actual_end_date'];
-$user_id = $
 
 if (isset($_POST['create_task'])) {
+  $id = getLoginSession();
+  createTask($id, $status, $title, $description, $created_at, 
+  $planned_start_date, $planned_end_date, $actual_start_date, 
+  $actual_end_date);
+}
 
+if (isset($_POST['edit'])) {
+  $task_id = $_POST['task_id'];
+  edit($task_id, $title, $description, $planned_start_date, 
+    $planned_end_date);
+}
+
+if (isset($_POST['delete_task'])) {
+  $task_id = $_POST['task_id'];
+  deleteTask($task_id);
+}
+
+if (isset($_POST['update_status'])) {
+  $task_id = $_POST['task_id'];
+  updatStatus($task_id,$status);
+}
+
+if (isset(['get_task'])) {
+  $user_id = getLoginSession();
+  getTask($user_id);
+}
+
+if (isset(['get_not_completed_tasks'])) {
+  getNotCompletedTasks($user_id);
+}
+
+if (isset(['get_completed_tasks'])) {
+  $user_id = getLoginSession();
+  getCompletedTasks($user_id);
+}
+
+if (isset($_POST['add_feedback'])) {
+  $comment = $_POST['comments'];
+  $task_id = $_POST['task_id'];
+  addFeedback($comment, $task_id);
 }
